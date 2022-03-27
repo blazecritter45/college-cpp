2d7be71a28b081cd6ecc82e5fe921d0a16a7b460
blazecritter45 <blazecritter45@gmail.com>
2022-03-27 19:26:27 +0530

    i tried

---
+++ greatest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  float a, b, c;
+  cout << "enter 3 numbers: ";
+  cin << a << b << c;
+
+  if((a >= b) && (a >= c))
+
+    cout << "largest number is " << a;
+
+  if((b >= c) && (b <= a))
+
+    cout << "largest number is " << b;
+
+
+  if((c >= a) && (c >= b))
+
+    cout << "largest number is " << c;
+
+  return 0;
+
+}
